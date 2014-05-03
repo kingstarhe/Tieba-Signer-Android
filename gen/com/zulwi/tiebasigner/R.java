6f6c93218806adba20bb25fbce4a1e39020e49ce
JerryLocke <i@jerrys.me>
2014-05-03 21:29:17 +0800

    Optimize code

--- gen/com/zulwi/tiebasigner/R.java
+++ gen/com/zulwi/tiebasigner/R.java
@@ -858,11 +858,12 @@ containing a value of this type.
         public static final int abc_split_action_bar_is_narrow=0x7f060002;
     }
     public static final class color {
-        public static final int abc_search_url_text_holo=0x7f070006;
+        public static final int abc_search_url_text_holo=0x7f070007;
         public static final int abc_search_url_text_normal=0x7f070000;
         public static final int abc_search_url_text_pressed=0x7f070002;
         public static final int abc_search_url_text_selected=0x7f070001;
         public static final int black=0x7f070005;
+        public static final int transparent=0x7f070006;
         public static final int white=0x7f070003;
         public static final int zulwi_blue=0x7f070004;
     }
@@ -1107,21 +1108,21 @@ containing a value of this type.
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
         public static final int activity_chooser_view_content=0x7f050025;
-        public static final int addSite=0x7f050040;
-        public static final int addSiteName=0x7f05003e;
-        public static final int addSiteUrl=0x7f05003f;
+        public static final int addSite=0x7f05003f;
+        public static final int addSiteName=0x7f05003d;
+        public static final int addSiteUrl=0x7f05003e;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
-        public static final int del_list=0x7f050046;
-        public static final int del_site=0x7f050048;
+        public static final int del_list=0x7f050045;
+        public static final int del_site=0x7f050047;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
-        public static final int edit_site=0x7f050047;
+        public static final int edit_site=0x7f050046;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
@@ -1132,12 +1133,12 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int login=0x7f050044;
+        public static final int login=0x7f050043;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int password=0x7f050043;
+        public static final int password=0x7f050042;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1155,18 +1156,17 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
-        public static final int site=0x7f050041;
-        public static final int siteList=0x7f05003d;
-        public static final int sitesListText=0x7f05003c;
+        public static final int site=0x7f050040;
+        public static final int siteList=0x7f05003c;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
-        public static final int url=0x7f050045;
+        public static final int url=0x7f050044;
         public static final int useLogo=0x7f050003;
-        public static final int username=0x7f050042;
+        public static final int username=0x7f050041;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
