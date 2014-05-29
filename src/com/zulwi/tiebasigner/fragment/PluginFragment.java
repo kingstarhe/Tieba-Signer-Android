349a3805e1a025bf1cc6b32d448c824ff678cdfc
JerryLocke <i@jerrys.me>
2014-05-29 08:18:20 +0800

    改进插件列表显示

--- src/com/zulwi/tiebasigner/fragment/PluginFragment.java
+++ src/com/zulwi/tiebasigner/fragment/PluginFragment.java
@@ -1,11 +1,25 @@
 package com.zulwi.tiebasigner.fragment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.zulwi.tiebasigner.R;
 import com.zulwi.tiebasigner.activity.MainActivity;
@@ -18,22 +32,21 @@ import com.zulwi.tiebasigner.util.ClientApiUtil;
 import com.zulwi.tiebasigner.util.UserCacheUtil;
 import com.zulwi.tiebasigner.view.ListTableView;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-
-public class PluginFragment extends Fragment {
+public class PluginFragment extends Fragment implements OnRefreshListener {
 	private ListTableView tiebaTable;
 	private MainActivity activity;
 	private AccountBean accountBean;
 	private boolean loadedFlag = false;
+	private Map<String, String> supportedPlugins = new HashMap<String, String>();
 	private List<PluginBean> pluginList = new ArrayList<PluginBean>();
+	{
+		supportedPlugins.put("zw_custom_page", "自定义页面");
+		supportedPlugins.put("zw_blockid", "循环封禁");
+		supportedPlugins.put("x_tdou", "T豆");
+		supportedPlugins.put("xxx_post", "客户端回帖");
+		supportedPlugins.put("xxx_meizi", "妹纸认证");
+	}
+
 	private class getPluginInfo extends Thread {
 		@Override
 		public void run() {
@@ -56,7 +69,7 @@ public class PluginFragment extends Fragment {
 			}
 		}
 	}
-	
+
 	@SuppressLint("HandlerLeak")
 	private Handler handler = new Handler() {
 		@Override
@@ -74,19 +87,19 @@ public class PluginFragment extends Fragment {
 					loadedFlag = true;
 					if (json.status == 0) {
 						JSONArray plugins;
-                        try {
-	                        plugins = json.data.getJSONArray("plugins");
+						try {
+							plugins = json.data.getJSONArray("plugins");
 							pluginList.clear();
-							for(int i=0;i<plugins.length();i++){
+							for (int i = 0; i < plugins.length(); i++) {
 								JSONObject plugin = plugins.getJSONObject(i);
-								pluginList.add(new PluginBean(plugin.getString("id"), plugin.getString("id"), plugin.getString("ver")));
+								pluginList.add(new PluginBean(plugin.getString("id"), plugin.getString("name"), plugin.getString("ver")));
 							}
 							PluginListAdapter adapter = new PluginListAdapter(activity, pluginList);
 							tiebaTable.setAdapter(adapter);
-                        } catch (JSONException ej) {
-                        	ej.printStackTrace();
-                        	tips = "JSON解析错误";
-                        }
+						} catch (JSONException ej) {
+							ej.printStackTrace();
+							tips = "JSON解析错误";
+						}
 					} else {
 						tips = json.message;
 					}
@@ -99,10 +112,12 @@ public class PluginFragment extends Fragment {
 					tips = t.getMessage();
 					break;
 			}
+			swipeLayout.setRefreshing(false);
 			if (tips != null && !tips.equals("")) Toast.makeText(activity, tips, Toast.LENGTH_SHORT).show();
 		}
 	};
-	
+	private SwipeRefreshLayout swipeLayout;
+
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
@@ -118,13 +133,17 @@ public class PluginFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_plugin, container, false);
+		swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_container);
+		swipeLayout.setOnRefreshListener(this);
+		swipeLayout.setColorScheme(R.color.holo_blue_bright, R.color.holo_green_light, R.color.holo_orange_light, R.color.holo_red_light);
 		tiebaTable = (ListTableView) view.findViewById(R.id.plugin_list);
 		tiebaTable.setOnClickListener(new ListTableView.onClickListener() {
 			@Override
 			public void onClick(View v, int which) {
+				Toast.makeText(activity, getString(R.string.maimeng) + " 这个功能还在开发中", Toast.LENGTH_SHORT).show();
 			}
 		});
-		activity.showLoadingDialog(true);
+		swipeLayout.setRefreshing(true);
 		new getPluginInfo().start();
 		return view;
 	}
@@ -132,6 +151,11 @@ public class PluginFragment extends Fragment {
 	@Override
 	public void onStart() {
 		super.onStart();
+	}
+
+	@Override
+	public void onRefresh() {
+		new getPluginInfo().start();
 	}
 
 }
